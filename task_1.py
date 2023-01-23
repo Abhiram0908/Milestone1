92eed88dcb538632fed69c4e35130be55d64fcd1
Abhi Ram <98797128+Abhiram0908@users.noreply.github.com>
2023-01-23 12:35:17 +0530

    Add files via upload

--- task_1.py
+++ task_1.py
@@ -1,18 +1,41 @@
-f = open("Format_Source.txt", "r")
-Find = 'boundary'
-dimen = 'xy'
-endel = 'endel'
+import linecache
+
+boundary = "boundary"
+dimen = "xy"
+endel = "endel"
 count = 0
+boun = []
+ende = []
 
-# Printing header part
+f = open("Format_Source.txt", "r")
+f1 = open("output.txt", "a")
+
+# printing hearder
 for x in f:
-    if Find in x:
-        print(x)
+    if boundary in x:
         break
+    print(x, file=f1)
+    
+f.close()
+f = open("Format_Source.txt", "r")
 
-            
-for x in f:
-    print(x)
-    if endel in x: 
-        break
+lines = f.readlines()
+for k in lines:
+    count = count + 1
+    
+    if boundary in k:
+        count1 = count
+        boun.append(count1)
+        
+print(*boun, sep = " ")
 
+for i in range(boun[0], boun[2]):
+    particular_line = linecache.getline("Format_Source.txt", i)
+
+    # print the particular line
+    print(particular_line, file = f1)
+    
+
+
+f.close()
+f1.close()
